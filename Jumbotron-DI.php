fd17922f7f9b31e6d76000a1fb542da1f13c99ab
Mike Files <38595588+mkefiles@users.noreply.github.com>
2025-02-07 14:18:20 -0500

    Delete Jumbotron-DI.php

--- Jumbotron-DI.php
+++
@@ -1,7 +0,0 @@
-<sl-jumbotron>
-    <sl-jumbotron__content>
-        <h1>Protecting Your Paycheck.</h1>
-        <p>Protect your most <b>valuable</b> asset in the event of an <b>unexpected</b> disability.</p>
-        <a class="btn btn--blue" href="https://calendly.com/securelegacyins/needs-analysis" target="_blank">SCHEDULE A MEETING</a>        
-    </sl-jumbotron__content>
-</sl-jumbotron>
