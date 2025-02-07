f499770b0eba91a6cd98513380d1aa7e6c031fb8
Mike Files <38595588+mkefiles@users.noreply.github.com>
2025-02-07 14:18:39 -0500

    Delete Jumbotron-WI.php

--- Jumbotron-WI.php
+++
@@ -1,7 +0,0 @@
-<sl-jumbotron>
-    <sl-jumbotron__content>
-        <h1>Protecting Your Employees.</h1>
-        <p>Protect the <b>workforce</b> that powers your <b>business</b>... at little-to-no cost to you.</p>
-        <a class="btn btn--blue" href="https://calendly.com/securelegacyins/needs-analysis" target="_blank">SCHEDULE A MEETING</a>        
-    </sl-jumbotron__content>
-</sl-jumbotron>
