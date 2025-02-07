83eb3596d12a8037aa6be0f7ae5bb55f6d042d66
Mike Files <38595588+mkefiles@users.noreply.github.com>
2025-02-07 14:20:15 -0500

    Delete Why.php

--- Why.php
+++
@@ -1,4 +0,0 @@
-<sl-why>
-    <h2>Why SecureLegacy?</h2>
-    <p>We are <b>independent</b> insurance agents who provide <em>white glove</em> service for your insurance needs. We work with multiple carriers to provide life insurance, disability income insurance (DI) and worksite insurance solutions that can be personalized to fit your specific needs and budget. While you cannot always predict what life will bring next, it's important to be ready for the future.</p>
-</sl-why>
