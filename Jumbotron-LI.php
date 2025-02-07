b07bc1452d04980f293cb29c61bb1b3aa8cfd027
Mike Files <38595588+mkefiles@users.noreply.github.com>
2025-02-07 14:18:29 -0500

    Delete Jumbotron-LI.php

--- Jumbotron-LI.php
+++
@@ -1,7 +0,0 @@
-<sl-jumbotron>
-    <sl-jumbotron__content>
-        <h1>Protecting Your Family.</h1>
-        <p>Protect the people you care about <b>most</b> for when the <b>worst</b> happens.</p>
-        <a class="btn btn--blue" href="https://calendly.com/securelegacyins/needs-analysis" target="_blank">SCHEDULE A MEETING</a>        
-    </sl-jumbotron__content>
-</sl-jumbotron>
