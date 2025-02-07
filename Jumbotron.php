577c96392a79de2b6509b91600c18517c1bcb518
Mike Files <38595588+mkefiles@users.noreply.github.com>
2025-02-07 14:18:51 -0500

    Delete Jumbotron.php

--- Jumbotron.php
+++
@@ -1,7 +0,0 @@
-<sl-jumbotron>
-    <sl-jumbotron__content>
-        <h1>Protecting What Matters Most.</h1>
-        <p>We can help protect <b>your</b> income, <b>your</b> family and <b>your</b> employees!</p>
-        <a class="btn btn--blue" href="https://calendly.com/securelegacyins/needs-analysis" target="_blank">SCHEDULE A MEETING</a>        
-    </sl-jumbotron__content>
-</sl-jumbotron>
